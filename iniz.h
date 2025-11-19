6e82151060c2712c0e8e9caef992e8570fe33b90
Roberto <43422746+ildibba@users.noreply.github.com>
2025-11-19 21:45:04 +0100

    Remove commented-out PCF8574 initialization code

--- iniz.h
+++ iniz.h
@@ -1,19 +1,1 @@
 
-
-
-
-
-/*
-
-Wire.begin(21, 22); // SDA=21, SCL=22
-  if (!pcf.begin()) {
-    Serial.println("Errore inizializzazione PCF8574!");
-    while (1);
-  }
-  
-  */
-  
-  
-Wire.begin(21, 22); // SDA=21, SCL=22
-
-pcf.begin();
